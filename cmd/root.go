4d604ee082cc32384022ebfe160bd3a5fcb023de
mkan0141 <kanzume0141@gmail.com>
2020-03-05 11:44:38 +0900

    Update: --u --o オプションの追加

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,10 @@
 package cmd
 
 import (
+	"fmt"
+	"time"
+
+	"github.com/mkan0141/gws/util"
 	"github.com/spf13/cobra"
 )
 
@@ -9,6 +13,18 @@ var RootCmd = &cobra.Command{
 	Short: "",
 	Long:  "",
 	Run: func(cmd *cobra.Command, args []string) {
-
+		fmt.Println(util.GetGrassSVG(user))
 	},
+}
+
+var (
+	user string
+	file string
+)
+
+func init() {
+	time.Now().Format("20060102")
+	RootCmd.Flags().StringVar(&user, "u", "", "GitHubのユーザ名(required)")
+	RootCmd.MarkFlagRequired("u")
+	RootCmd.PersistentFlags().StringVar(&file, "o", time.Now().Format("20060102"), "出力ファイル名")
 }
